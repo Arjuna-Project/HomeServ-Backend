7e4000b4e0022b0871173c99682b3c8f0dea0cf1
Arjuna-Project <arjunvk1819@gmail.com>
2026-01-05 23:27:26 +0530

    CORS Error debug1

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="HomeServ API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
+        "https://home-serv-frontend.vercel.app",
         "http://localhost:5500",
     ],
     allow_credentials=True,
